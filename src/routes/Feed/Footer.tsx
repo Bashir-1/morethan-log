390899ef75200a559686964d8e2e81cfa5804283
NCC-17039 <120711536+Bashir-1@users.noreply.github.com>
2023-12-27 18:20:38 +0800

    Update Footer.tsx

--- src/routes/Feed/Footer.tsx
+++ src/routes/Feed/Footer.tsx
@@ -14,7 +14,7 @@ const Footer: React.FC<Props> = ({ className }) => {
   return (
     <StyledWrapper className={className}>
       <a
-        href={`https://github.com/${CONFIG.profile.github}`}
+        href={`https://starshine.love/`}
         target="_blank"
         rel="noreferrer"
       >
