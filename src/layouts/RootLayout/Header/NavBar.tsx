271609006a10626a912f2d06bff9187d10857a5f
NCC-17039 <120711536+Bashir-1@users.noreply.github.com>
2023-12-28 09:28:47 +0800

    Update NavBar.tsx

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -2,7 +2,13 @@ import styled from "@emotion/styled"
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "About", to: "/about" }]
+  const links = [
+    { id: 1, name: "关于", to: "/about" },
+    { id: 2, name: "友链", to: "/links" },
+    { id: 3, name: "日志", to: "/update" }, 
+    { id: 4, name: "联系", to: "/contact" }, 
+    { id: 5, name: "站点订阅", to: "/sitemap.xml" }, 
+  ];
   return (
     <StyledWrapper className="">
       <ul>
