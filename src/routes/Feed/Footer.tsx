b64da63ceef817affc67220813a55175d2bebbb2
NCC-17039 <120711536+Bashir-1@users.noreply.github.com>
2023-12-28 11:04:42 +0800

    Update Footer.tsx

--- src/routes/Feed/Footer.tsx
+++ src/routes/Feed/Footer.tsx
@@ -20,7 +20,6 @@ const Footer: React.FC<Props> = ({ className }) => {
       >
         © {CONFIG.profile.name} {from === y || !from ? y : `${from} - ${y}`}
       </a>
-      <br>博客所有文章除特别声明外，均采用署名-非商业性使用-相同方式共享 4.0 国际(CC BY-NC-SA 4.0)许可协议；转载文章请注明来自：NCC-17039（附上本站地址）</br>
     </StyledWrapper>
   )
 }
